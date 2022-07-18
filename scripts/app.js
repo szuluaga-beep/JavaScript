6393f746e0b7febf665e41b862d9124f4d1a59e4
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-07-18 09:26:16 -0500

    Creamos una constante y vimos variable undefined

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+let nombre=prompt("Ingrese su Nombre");
+let apellido =prompt("Ingrese su Apellido");
+
+console.log(nombre+apellido);
+// Este es una constante
+const email="steven@yopmail.com";
+console.log(`El correo es: ${email}`);
+
+//Este esun ejemplo de undefined
+let correo;
+console.log(`El correo es: ${correo}`);
