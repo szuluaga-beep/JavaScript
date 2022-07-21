246379a349fe92b21a563a094a2ef48e80554ed0
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-07-21 08:09:35 -0500

    Esta es la clase 3 sobre arrays y objetos

---
+++ Clase 3/assets/app.js
@@ -0,0 +1,12 @@
+//crear array vacio
+let array=[];
+
+console.log(array);
+//Creamos cuatro vectores con diferentes valores
+let numeros=[1,2,3,4]
+//console.log(numeros)
+let animales=["perro","gato","loro","pato","sapo"]
+//console.table(animales)
+let colores=["rojo","verde","amarillo","negro"]
+//console.log(colores)
+console.log(colores.length)
