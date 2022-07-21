8ecb7a1d0c5161d67bdb780a9207a1a89c972b1b
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-07-21 09:17:11 -0500

    En esta sección vimos objetos

---
+++ Clase 3/assets/objetos.js
@@ -0,0 +1,28 @@
+//Crear objeto vacio
+let objeto={}
+console.log(objeto)
+// Creando objeto carro
+let carro={
+    color:"rojo",
+    velocidadMaxima:"100 km/h",
+    modelo:2020,
+    placa:"ABC123",
+    propietarios:["Steven","Yinier"],
+    lugarCreacion:{
+        ciudad:"Medellin",
+        pais:"Colombia"
+    },
+    tieneSoat:true
+}
+console.log(carro)
+
+const viche={
+    color:"transparente",
+    valor:5000,
+    productoHecho:"Caña de azucar",
+    gradoAlcohol:"15%",
+    esAfrodisiaco:false
+}
+console.log(`El precio del viche es: ${viche.valor}`)
+console.log(`El viche esta hecho de ${viche.productoHecho}`)
+
