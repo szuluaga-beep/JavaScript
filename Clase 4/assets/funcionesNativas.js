598f3835c281cb87a4b4955bf853a000724a40a8
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-07-22 09:21:55 -0500

    Funciones nativas para strings

---
+++ Clase 4/assets/funcionesNativas.js
@@ -0,0 +1,15 @@
+let saludo="Hola Steven"
+
+console.log(saludo);
+console.log(saludo.length)//Devuelve la cantidad de caracteres
+console.log(saludo.toUpperCase())//Devuelve el texto en mayúsculas
+console.log(saludo.toLowerCase())// Devuelve el texto en minúsculas
+console.log(saludo.slice(4,8))//Devuelve el texto comprendido entre las posiciones proporcionadas por parámtero
+console.log(saludo.includes("Steven"))
+console.log(saludo.indexOf("a"))
+console.log(saludo.replace("a","@"))
+
+let numero=5
+
+console.log(typeof numero.toString())
+console.log(typeof numero)
