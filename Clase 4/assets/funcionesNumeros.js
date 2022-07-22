98f1585633df6452aea81a8ee1bc306968be3aaa
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-07-22 09:56:28 -0500

    Funciones nativas para números

---
+++ Clase 4/assets/funcionesNumeros.js
@@ -0,0 +1,22 @@
+console.log(Math.E)//Devuelve la constante de Euler
+console.log(Math.PI)
+
+console.log(Math.pow(2,2))
+console.log(2**3)
+
+let numero1=30;
+let numero2=82;
+
+console.log(Math.min(numero1,numero2))
+console.log(Math.max(numero1,numero2))
+
+const numeroPI=3.1416
+
+console.log(Math.round(numeroPI))
+console.log(Math.ceil(numeroPI))
+
+console.log(Math.random()*(numero2-numero1)+numero1)
+console.log(Math.trunc(Math.random()*(numero2-numero1)+numero1))
+
+
+const numero=[]
