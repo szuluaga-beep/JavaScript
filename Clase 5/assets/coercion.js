f7526d63ed1bb2c106ab27b14d6d54648d2dea14
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-07-25 09:18:48 -0500

    Vimos coercion o conversión de tipos de datos

---
+++ Clase 5/assets/coercion.js
@@ -0,0 +1,22 @@
+let num1=2,num2="3"
+
+let resultado=num1+num2
+
+console.log(resultado)
+console.log(typeof resultado)
+
+let resultadoEntero=num1+Number(num2);
+
+console.log(resultadoEntero)
+
+let res=num1+parseInt(num2)
+
+console.log(res)
+
+let nota1=3.5,nota2="4.5"
+
+let suma=nota1+parseFloat(nota2)
+
+console.log(suma)
+
+console.log(typeof suma.toString())
