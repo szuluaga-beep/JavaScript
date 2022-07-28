20005eaef8bd0fd741598bf7aaccf84dc76adee5
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-07-28 07:24:41 -0500

    Aqui vimos el operador &&

---
+++ Clase 7/assets/app.js
@@ -0,0 +1,16 @@
+//Aquí vimos el operador &&
+const entrarAlBar = (edad, pago) => {
+  if (edad >= 18 && pago == true) {
+    console.log("Puede entrar al bar");
+  } else {
+    console.log("No puede entrar al bar");
+  }
+};
+let edadPersona = Number(prompt("Ingrese su edad"));
+let pagoEntrada = Boolean(prompt("¿¿Usted pago??"));
+entrarAlBar(edadPersona, pagoEntrada);
+
+console.log(true && true);
+console.log(true && false);
+console.log(false&&true);
+console.log(false && false);
