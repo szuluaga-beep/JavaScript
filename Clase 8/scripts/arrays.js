10926f52e1a668eeeefeada6f49f82918098f43e
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-08-01 10:00:42 -0500

    Metodos de los arrays

---
+++ Clase 8/scripts/arrays.js
@@ -0,0 +1,15 @@
+let numeros=[2,3,5,8,6,1]
+
+console.log(numeros)
+//console.log(numeros.sort())
+
+numeros.push(10)
+
+console.log(numeros)
+
+numeros.pop()
+
+console.log(numeros)
+
+numeros.unshift(10)
+console.log(numeros.length)
