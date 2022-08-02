5dfed10478ad6b8ec24fad8f5e51d3915cadb116
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-08-02 09:22:34 -0500

    Alcance de las variabñes

---
+++ Clase 9/scripts/alcance.js
@@ -0,0 +1,22 @@
+let num=1;
+console.log(num)
+const saludar=()=>{
+    let num2=5;
+    console.log(num*num2)
+}
+saludar()
+
+
+const validarEdad=()=>{
+    let edad=17;
+    if (edad>18) {
+        let mensaje="Wakanda for ever"
+        console.log("Es mayor de edad")
+        console.log(mensaje)
+    }else{
+        //console.log(mensaje)
+        console.log("Estas sardino")
+    }
+}
+
+validarEdad()
