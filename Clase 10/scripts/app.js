d59beb419d5138ed03444c29d34bd4123a10888c
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-08-04 07:34:00 -0500

    Aqui vimos como obtener datos con el nombre de la etiqueta

---
+++ Clase 10/scripts/app.js
@@ -0,0 +1,7 @@
+//Obtener elementos con la funcion getElementsByTagName
+let todosTitulos=document.getElementsByTagName('h1')
+
+console.log(todosTitulos)
+
+let todosSubtitulos=document.getElementsByTagName('h2')
+console.log(todosSubtitulos)
