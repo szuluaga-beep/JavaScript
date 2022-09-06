7b666ed53706a8530b72480c30854cccc5fd91da
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-09-06 10:02:06 -0500

    Aqui vimos Webpack

---
+++ Clase 23/src/index.js
@@ -0,0 +1,10 @@
+
+
+const crearTitulo=()=>{
+    const titulo=document.createElement("h1")
+    titulo.innerHTML="Hola Mundo"
+
+    document.body.appendChild(titulo)
+}
+
+crearTitulo()
---
+++ Clase 23/webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports={
+    mode:'development',
+    module:{
+        rules:[
+            {
+                test:/\.html$/,
+                loader:"html-loader",
+                options:{
+                    sources:false
+                }
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:"./index.html"
+        })
+    ]
+
+}
